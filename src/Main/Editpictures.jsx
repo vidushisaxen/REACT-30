71708cea5ca5fa0845337d5a6eaa44023c3241e1
vidushisaxen <vidushisaxenabdn786@gmail.com>
2023-01-06 22:57:53 +0530

    main

--- src/Main/Editpictures.jsx
+++ src/Main/Editpictures.jsx
@@ -14,12 +14,19 @@ function Editpictures() {
     <div className="editmain">
         <label>Blur</label>
         <input></input>
+        <br></br>
         <label>Spacing</label>
         <input></input>
+        <br></br>
         <label>Border</label>
         <input></input>
+        <br></br>
         <label>Color</label>
         <input></input>
+        <br></br>
+    </div>
+    <div className="editimg">
+        <img src='https://media.istockphoto.com/id/809971888/photo/night-sky-landscape.jpg?b=1&s=170667a&w=0&k=20&c=-1smAP9--twhHyqopksRe3lIORQvO9JaaNWbT1Ra-Is='></img>
     </div>
     
     </>
