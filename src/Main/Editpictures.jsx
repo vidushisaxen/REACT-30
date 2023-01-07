5e3fc14171eb60a0589408741384ecc57e73d839
vidushisaxen <vidushisaxenabdn786@gmail.com>
2023-01-07 20:46:40 +0530

    main

--- src/Main/Editpictures.jsx
+++ src/Main/Editpictures.jsx
@@ -4,29 +4,27 @@ import './Editpictures.css';
 function Editpictures() {
   return (
     <>
+    <h1>Let's be creative</h1>
     <div className="edit">
-        <h1>Let's edit pictures</h1> 
-        <label>Image link</label>
+        
+        <div className="left">
+        <label>Image link :</label>
         <input></input>
-    </div>
-    
-    <br></br><br></br>
-    <div className="editmain">
-        <label>Blur</label>
+        <label>Blur :</label>
         <input></input>
-        <br></br>
-        <label>Spacing</label>
+        <label>Spacing :</label>
         <input></input>
-        <br></br>
-        <label>Border</label>
+        <label>Border :</label>
         <input></input>
-        <br></br>
-        <label>Color</label>
+        <label>Color :</label>
         <input></input>
-        <br></br>
-    </div>
-    <div className="editimg">
-        <img src='https://media.istockphoto.com/id/809971888/photo/night-sky-landscape.jpg?b=1&s=170667a&w=0&k=20&c=-1smAP9--twhHyqopksRe3lIORQvO9JaaNWbT1Ra-Is='></img>
+        </div>
+
+
+
+        <div className="right">
+        <img src='https://media.istockphoto.com/id/809971888/photo/night-sky-landscape.jpg?b=1&s=170667a&w=0&k=20&c=-1smAP9--twhHyqopksRe3lIORQvO9JaaNWbT1Ra-Is='></img></div> 
+        
     </div>
     
     </>
