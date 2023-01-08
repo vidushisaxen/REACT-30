466277c91e20c29b0c677782fd85f0389a33f9f9
vidushisaxen <vidushisaxenabdn786@gmail.com>
2023-01-08 20:42:47 +0530

    main

--- src/Main/Editpictures.jsx
+++ src/Main/Editpictures.jsx
@@ -3,6 +3,30 @@ import './Editpictures.css';
 
 function Editpictures() {
     const [imglink, setimglink] = useState("https://media.istockphoto.com/id/809971888/photo/night-sky-landscape.jpg?b=1&s=170667a&w=0&k=20&c=-1smAP9--twhHyqopksRe3lIORQvO9JaaNWbT1Ra-Is=");
+    const[value , setValue] = useState("");
+    const[value1 , setValue1] = useState("");
+
+    function changeblur(event)
+    { 
+       setValue(event.target.value);
+       console.log(value)
+     document.documentElement.style.setProperty('--blur' , value+"px");
+    }
+   
+   function changespacing(event)
+   {
+       setValue1(event.target.value);
+     document.documentElement.style.setProperty('--spacing' , value1+"px");
+   
+   }
+   function changecolor(event)
+   {
+       document.documentElement.style.setProperty('--base' , event.target.value);
+   }
+   function changeradius(event)
+   {
+       document.documentElement.style.setProperty('--radius' , event.target.value+"px");
+   }
   return (
     <>
     <div className="editbody">
@@ -13,13 +37,13 @@ function Editpictures() {
         <label>Image link :</label>
         <input onChange={(event)=>setimglink(event.target.value)}></input>
         <label>Blur :</label>
-        <input></input>
+        <input type={"range"} min = {"-1"} max = {"100"}  onChange = {changeblur}></input>
         <label>Spacing :</label>
-        <input></input>
-        <label>Border :</label>
-        <input></input>
-        <label>Color :</label>
-        <input></input>
+        <input type={"range"} min = {"0"} max = {"100"} onChange = {changespacing} ></input>
+        <label>Border radius :</label>
+        <input type={"range"} min = {"0"} max = {"100"} onChange = {changeradius}></input>
+        <label>Base color :</label>
+        <input type={"color"}  onChange = {changecolor}  ></input>
         </div>
 
 
