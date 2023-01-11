498def56d1c4c5ca96661a0620078bfa5537e768
vidushisaxen <vidushisaxenabdn786@gmail.com>
2023-01-11 21:59:05 +0530

    main

--- src/Main/Wallpaper.jsx
+++ src/Main/Wallpaper.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import {React,useState} from 'react'
 import './Wallpaper.css'
+import axios from 'axios';
 
 function Wallpaper() {
+  const [state, setstate] = useState([])
+  const [search, setsearch] = useState("")
+
+  async function apicall(term){
+    
+  
+      const response = await axios.get('https://api.unsplash.com/search/photos',{
+    params : {query : term},  
+    headers : {
+      Authorization: 'Client-ID m3gmLiCbu9gFm3woo0qpMpZlZ-w06Ky3rDwRzGwTI64'
+    }
+    
+    
+  });
+  console.log(response.data.results)
+  setstate(response.data.results);
+    }
+  
   return (
     <>
     <div className="wall">
-        <h1>Get HD Wallpapers on one click!</h1>
+      <h1>Backdrops</h1>
+        <input placeholder='Search' onChange={(e)=>{setsearch(e.target.value)}}></input>
+        <button onClick={(()=>{apicall(search)})}>Search</button>
+        {state.map((data)=>{return(
+          <>
+          <img src={data.urls.regular}></img>
+          </>
+        )})}
+
     </div>
     </>
   )
