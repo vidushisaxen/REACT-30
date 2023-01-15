d4a93516ebca48ec88b13410708198776bc70652
vidushisaxen <vidushisaxenabdn786@gmail.com>
2023-01-15 22:02:01 +0530

    main

--- src/Main/Flexgallery.jsx
+++ src/Main/Flexgallery.jsx
@@ -2,22 +2,49 @@ import React from 'react'
 import './Flex.css'
 
 function Flexgallery() {
+const panels= document.querySelectorAll('.panels');
+
+function toggleopen(){
+    this.classList.toggle('open');
+}
+function toggleactive(e){
+    
+    if(e.propertyName.includes('flex'));
+    this.classList.toggle('open-active');
+}
+panels.forEach(panel=>panel.addEventListener('click',toggleopen));
+panels.forEach(panel=>panel.addEventListener('transitioned',toggleactive));
+
+
   return (
     <>
-    <div className="panel panelone">
+    <div className="panels">
+    <div className="panel panel1">
+        <p>Be</p>
+        <p>The</p>
+        <p>Change</p>
        
     </div>
-    <div className="panel paneltwo">
-       
+    <div className="panel panel2">
+       <p>Believe</p>
+       <p>In</p>
+       <p>Yourself</p>
     </div>
-    <div className="panel panelthree">
-       
+    <div className="panel panel3">
+       <p>Change</p>
+       <p>Is</p>
+       <p>Good</p>
     </div>
-    <div className="panel panelfour">
-       
+    <div className="panel panel4">
+        <p>Dreams</p>
+        <p>Come</p>
+       <p>True</p>
     </div>
-    <div className="panel panelfive">
-       
+    <div className="panel panel5">
+        <p>Focus</p>
+       <p>And</p>
+       <p>Win</p>
+    </div>
     </div>
     </>
   )
