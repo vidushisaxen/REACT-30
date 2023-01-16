420ae8c677bf6ec9bbfa0861d64d2002e6388059
vidushisaxen <vidushisaxenabdn786@gmail.com>
2023-01-16 16:45:17 +0530

    main

--- src/Main/Flexgallery.jsx
+++ src/Main/Flexgallery.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import './Flex.css'
 
 function Flexgallery() {
+    const [state,setState] = React.useState("panel panel2");
+
+const [state1,setState1] = React.useState("panel panel1");
+
+const [state2,setState2] = React.useState("panel panel3");
+
+const [state3,setState3] = React.useState("panel panel4");
 const panels= document.querySelectorAll('.panels');
 
 function toggleopen(){
