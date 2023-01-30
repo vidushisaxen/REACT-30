497e29f6a6cd343d4c2af00a613042c5c0980f52
vidushisaxen <vidushisaxenabdn786@gmail.com>
2023-01-30 20:21:36 +0530

    main

--- src/Main/Flexgallery.jsx
+++ src/Main/Flexgallery.jsx
@@ -36,28 +36,28 @@ function Flexgallery() {
     <div className="panel panel1">
         <p>Be</p>
         <p>The</p>
-        <p>Change</p>
+        <p>Change.</p>
        
     </div>
     <div className="panel panel2">
        <p>Believe</p>
        <p>In</p>
-       <p>Yourself</p>
+       <p>Yourself.</p>
     </div>
     <div className="panel panel3">
        <p>Change</p>
        <p>Is</p>
-       <p>Good</p>
+       <p>Good.</p>
     </div>
     <div className="panel panel4">
         <p>Dreams</p>
         <p>Come</p>
-       <p>True</p>
+       <p>True.</p>
     </div>
     <div className="panel panel5">
         <p>Focus</p>
        <p>And</p>
-       <p>Win</p>
+       <p>Win.</p>
     </div>
     </div>
     </>
