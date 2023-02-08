9cc0eab59957897cf5326ad5ec8ad3bcead79dd3
vidushisaxen <vidushisaxenabdn786@gmail.com>
2023-02-08 22:39:13 +0530

    main

--- src/Main/Cities.jsx
+++ src/Main/Cities.jsx
@@ -1,5 +1,19 @@
 import React from 'react'
 import './Cities.css'
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
+function Search() {
+    const[citydata,setData] = useState("");
+    const[searchvalue ,setSearchvalue] = useState("");
+    const[recommend , setRecommed] = useState([]);
+     async function call()
+    {
+        const datafile=   await axios("https://raw.githubusercontent.com/nshntarora/Indian-Cities-JSON/master/cities.json");
+        setData(datafile.data);
+          console.log(citydata)
+         
+    }
+}
 
 function Cities() {
   return (
