9c8ceffb1c499117edbf7db615d478dc0756c5ab
vidushisaxen <vidushisaxenabdn786@gmail.com>
2023-02-20 23:55:19 +0530

    main

--- src/Main/Stickynav.jsx
+++ src/Main/Stickynav.jsx
@@ -6,7 +6,7 @@ function Stickynav() {
     <>
     <div className='stick'>
     <div className="stickimg">
-        <img src='https://c4.wallpaperflare.com/wallpaper/604/259/798/black-and-white-life-wallpaper-preview.jpg'></img>
+        <img src='https://wallpapershome.com/images/wallpapers/lake-3840x2160-4k-hd-wallpaper-sea-pink-sunset-sunrise-reflection-sky-864.jpg'></img>
     </div>
     <div className="sticknav">
         <p1>
@@ -25,9 +25,16 @@ function Stickynav() {
             Maps
         </p1>
     </div>
-    <div className="stictxtbox">
-        
+    <div className="sticktxtbox">
+     <p>The adventure of living in the path of life. We are born, live our lives, and eventually pass away with time. We are attempting to shape our lives in this way. Everyone's life is different. Some people have a lot of problems in life, while others do not. Those who have never faced adversity in their lives have one perspective on life. Those that struggle in life have a different perspective. Life is frequently described as priceless.
+        <br></br><br></br>
+        One important aspect of Life is that it keeps going forward. This means nothing is permanent. Hence, there should be a reason to stay in dejection. A happy occasion will come to pass, just like a sad one. Above all, one must be optimistic no matter how bad things get. This is because nothing will stay forever. Every situation, occasion, and event shall pass. This is certainly a beauty of Life.
+        <br></br>
+        <br></br>
+        The uncertainty of death is what makes life so precious. No one knows the hour of one’s death. This probably is the most important reason to live life to the fullest.
+     </p>
     </div>
+    <br></br>
     </div>
     </>
   )
